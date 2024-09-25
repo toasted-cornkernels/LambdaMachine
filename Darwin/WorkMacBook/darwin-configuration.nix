060365b717cd39af452f2ab455da76e5ea8293a8
toasted-cornkernels <volcano0909@gmail.com>
2024-09-24 19:04:50 -0700

    update

--- Darwin/WorkMacBook/darwin-configuration.nix
+++ Darwin/WorkMacBook/darwin-configuration.nix
@@ -1,26 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  imports = [ ../common-configuration.nix ];
   environment.systemPackages = [ pkgs.home-manager ];
   environment.darwinConfig = "$HOME/LambdaMachine/Darwin/WorkMacBook/darwin-configuration.nix";
-
-  services.nix-daemon.enable = true;
-  nix = {
-    package = pkgs.nix;
-    settings = { "extra-experimental-features" = [ "nix-command" "flakes" ]; };
-  };
-
-  programs = {
-    gnupg.agent.enable = true;
-    zsh = { enable = true; };
-  };
-
-  security.pam.enableSudoTouchIdAuth = true;
-
-  nixpkgs.config.allowUnfree = false;
-  nixpkgs.config.allowUnsupportedSystem = false;
-
-  system.stateVersion = 4;
 
   homebrew = {
     enable = true;
@@ -36,34 +19,11 @@
           "with-xwidgets"
         ];
       }
-      "blueutil"
     ];
-    casks = [ "hammerspoon" "zoom" ];
+    casks = [
+      "hammerspoon"
+      "alfred"
+    ];
     masApps = { };
-  };
-
-  system = {
-    keyboard = { remapCapsLockToControl = true; };
-    defaults = {
-      dock = {
-        autohide = false;
-        orientation = "bottom";
-        mru-spaces = true;
-        show-recents = false;
-        show-process-indicators = false;
-        wvous-tl-corner = 1;
-        wvous-tr-corner = 1;
-        wvous-bl-corner = 1;
-        wvous-br-corner = 1;
-      };
-      spaces = { spans-displays = false; };
-      finder = {
-        AppleShowAllExtensions = true;
-        AppleShowAllFiles = true;
-      };
-      NSGlobalDomain = { "com.apple.mouse.tapBehavior" = 1; };
-      trackpad = { TrackpadThreeFingerDrag = true; };
-    };
-    startup = { chime = false; };
   };
 }
--- Darwin/WorkMacBook/home.nix
+++ Darwin/WorkMacBook/home.nix
@@ -4,167 +4,35 @@ rec {
   nixpkgs = { config = { allowUnfree = false; }; };
 
   imports = [
-    ../../Common/programs/tmux.nix
-    ../../Common/programs/zsh.nix
-    ../../Common/programs/zoxide.nix
+    ../common-home.nix
+
     ../../Common/programs/fzf.nix
     ../../Common/programs/htop.nix
+    ../../Common/programs/tmux.nix
+    ../../Common/programs/zoxide.nix
+    ../../Common/programs/zsh.nix
+
+    ../../Common/packages/PL/Go.nix
+    ../../Common/packages/PL/Ruby.nix
+    ../../Common/packages/PL/Python.nix
+    ../../Common/packages/PL/Yaml.nix
+
+    ../../Common/packages/Utils/Compress.nix
+    ../../Common/packages/Utils/Dev.nix
+    ../../Common/packages/Utils/Mac.nix
+    ../../Common/packages/Utils/Search.nix
+    ../../Common/packages/Utils/Shell.nix
+    ../../Common/packages/Utils/Stats.nix
+    ../../Common/packages/Utils/Unix.nix
+    ../../Common/packages/Utils/VC.nix
+    ../../Common/packages/Utils/Web.nix
+
     ../programs/zsh.nix
   ];
 
   home.username = "jslee";
   home.homeDirectory = "/Users/${home.username}";
-  home.stateVersion = "23.11";
-  home.packages = with pkgs;
-    [
-      abcl
-      ack
-      agda
-      ammonite
-      asdf
-      aspell
-      autoconf
-      automake
-      babashka
-      bat
-      blueutil
-      boost
-      cask
-      chez
-      chicken
-      cloc
-      clojure
-      clojure-lsp
-      cmake
-      commitizen
-      coreutils
-      coursier
-      crystal
-      cscope
-      ctags
-      curl
-      dafny
-      delta
-      deno
-      dotnet-sdk_8
-      duf
-      dust
-      ecl
-      edencommon
-      elixir
-      eza
-      fd
-      ffmpeg
-      fizz
-      fnlfmt
-      fswatch
-      gambit
-      gcc
-      gh
-      git
-      git-lfs
-      glib
-      gmp
-      gnumake
-      gnupg
-      gnutar
-      go
-      gobject-introspection
-      gopls
-      gotop
-      gpgme
-      gradle
-      graphicsmagick
-      graphviz
-      gtk3
-      guile
-      harfbuzz
-      htop
-      hunspell
-      idris
-      idris2
-      iina
-      imagemagick
-      isync
-      janet
-      kotlin
-      kotlin-language-server
-      ktlint
-      leiningen
-      lfe
-      libass
-      libffi
-      libgccjit
-      libheif
-      libtool
-      llvm
-      lua-language-server
-      lzlib
-      m-cli
-      maven
-      mermaid-cli
-      moreutils
-      mpc-cli
-      mpfr
-      mu
-      mysql
-      nil
-      nim
-      ninja
-      nixfmt-classic
-      nodejs
-      onefetch
-      opam
-      openblas
-      pandoc
-      pango
-      pangolin
-      perl
-      pinentry_mac
-      pkg-config
-      plantuml
-      poppler
-      postgresql
-      powershell
-      python312
-      racket-minimal
-      rakudo
-      readline
-      redis
-      ripgrep
-      rlwrap
-      roswell
-      ruby
-      rust-analyzer
-      sbcl
-      sbt
-      scala
-      silver-searcher
-      skim
-      smartmontools
-      swiProlog
-      terminal-notifier
-      tesseract
-      texlab
-      tldr
-      tokei
-      tree
-      wangle
-      watchman
-      wget
-      xapian
-      yarn
-      z3
-      zip
-      (lua5_3_compat.withPackages (ps:
-        with ps; [
-          fennel
-          luafilesystem
-          readline
-          luasocket
-        ]))
-    ] ++ perl538Packages ++ python312Packages;
-  # home.file = {
+    # home.file = {
   #   "ELispMachine" = { # TODO submodule ELispMachine
   #     source = "TODO";
   #     recursive = true;
@@ -182,17 +50,4 @@ rec {
   #   #  recursive = true;
   #   #};
   # };
-  home.sessionVariables = {
-    EDITOR = "nvim";
-    PROMPT = "%(?.%F{green}√.%F{red}?%?)%f %B%F{240}%1~%f%b %# ";
-    CLICOLOR = 1;
-    LC_ALL = "en_US.UTF-8";
-    LANG = "en_US.UTF-8";
-    LANGUAGE = "en_US.UTF-8";
-    TEST = 1;
-  };
-  programs = {
-    home-manager = { enable = true; };
-    vim = { enable = true; };
-  };
 }
