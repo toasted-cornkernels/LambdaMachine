315d38f672602667c6484951be7bb6f9bef79e8f
toasted-cornkernels <volcano0909@gmail.com>
2024-12-28 14:24:06 -0800

    initial flake.nix

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Nixos config flake";
+  inputs = {
+    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-24.11-darwin";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      darwin.inputs.url.follows = "nixpkgs-darwin";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    home-manager-darwin = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
+    };
+  };
+
+  outputs = { self, nixpkgs-darwin, nixpkgs-unstable, home-manager-darwin
+    , darwin, ... }@inputs: {
+      darwinConfigurations = {
+        MacBook14M1Max = darwin.lib.darwinSystem {
+          specialArgs = { inherit inputs; };
+          system = "aarch64-darwin";
+          modules = [
+            ./Darwin/MacBook14M1Max/darwin-configration.nix
+            home-manager-darwin.darwinModules.home-manager
+            {
+              home-manager-darwin.useGlobalPkgs = true;
+              home-manager-darwin.useUserPkgs = true;
+              home-manager-darwin.users.jslee =
+                import ./Darwin/MacBook14M1Max/home.nix;
+            }
+          ];
+        };
+      };
+    };
+}
