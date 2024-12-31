0fc31edc8b067f1ed40768566d84cfc740ba5afe
toasted-cornkernels <volcano0909@gmail.com>
2024-12-31 13:30:35 -0800

    update

--- Common/modules/starship.nix
+++ Common/modules/starship.nix
@@ -4,22 +4,7 @@
     enable = true;
     enableZshIntegration = true;
     settings = {
-      format = ''[░▒▓](#a3aed2)\
-               [  ](bg:#a3aed2 fg:#090c0c)\
-               [](bg:#769ff0 fg:#a3aed2)\
-                 $directory\
-                   [](fg:#769ff0 bg:#394260)\
-                   $git_branch\
-                     $git_status\
-                     [](fg:#394260 bg:#212736)\
-                     $nodejs\
-                       $rust\
-                       $golang\
-                       $php\
-                       [](fg:#212736 bg:#1d2230)\
-                       $time\
-                         [ ](fg:#1d2230)\
-                         \n$character'';
+      format = "[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$nodejs$rust$golang[](fg:#212736 bg:#1d2230)$time[ ](fg:#1d2230)";
       character = {
         format = "$symbol";
         error_symbol = "[ ](bold fg:red bg:#19172C)";
@@ -27,7 +12,7 @@
       };
 
       directory = {
-        format = "[   $path ](fg:#e3e5e5 bg:#769ff0)[](fg:#2D2B40)";
+        format = "[   $path ](fg:#e3e5e5 bg:#769ff0)(fg:#2D2B40)";
         truncation_length = 3;
         truncation_symbol = "…/";
         substitutions = {
@@ -38,7 +23,7 @@
         };
       };
 
-      f     git_branch = {
+      git_branch = {
         format = "[  $branch ](fg:#769ff0 bg:#394260)";
       };
 
