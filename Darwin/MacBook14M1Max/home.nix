48eadb59a109fd84e1e8f080af3859ad147b3df6
Jeongsoo Lee <jeongsoolee09@github.com>
2025-01-13 12:28:54 -0800

    Add Fonts

--- Darwin/MacBook14M1Max/home.nix
+++ Darwin/MacBook14M1Max/home.nix
@@ -60,6 +60,7 @@ rec {
     ../../Common/packages/Utils/Dev.nix
     ../../Common/packages/Utils/Encrypt.nix
     ../../Common/packages/Utils/Financial.nix
+    ../../Common/packages/Utils/Fonts.nix
     ../../Common/packages/Utils/Mac.nix
     ../../Common/packages/Utils/Mail.nix
     ../../Common/packages/Utils/Media.nix
