2e80e52c03623f647a473251bcaa45b14eaa5ce9
Jeongsoo Lee <jeongsoolee09@github.com>
2025-01-16 12:39:24 -0800

    update

--- Common/packages/PL/Lua.nix
+++ Common/packages/PL/Lua.nix
@@ -1,14 +1,20 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     lua-language-server
-    (lua5_4_compat.withPackages (ps:
-      with ps; [
-        fennel
-        luafilesystem
-        readline
-        luasocket
-        luarocks
-      ]))
+    # (lua5_3_compat.withPackages (ps:
+    #   with ps; [
+    #     fennel
+    #     luafilesystem
+    #     readline
+    #     luasocket
+    #     luarocks
+    #   ]))
+    lua
+    fennel
+    luafilesystem
+    readline
+    luasocket
+    luarocks
     stylua
   ];
 }
