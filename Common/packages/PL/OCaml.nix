78ff1df2b8b901656f432ec01ffc25c11af38e33
toasted-cornkernels <volcano0909@gmail.com>
2025-03-15 16:19:54 -0400

    update

--- Common/packages/PL/OCaml.nix
+++ Common/packages/PL/OCaml.nix
@@ -4,13 +4,13 @@
     ocaml
     opam # TEMP
     dune_3
-    # ocamlPackages.ocaml-lsp
+    ocamlPackages.ocaml-lsp
     # ocamlPackages.core
     # ocamlPackages.utop
     # ocamlPackages.yojson
     # ocamlPackages.yaml
     # ocamlPackages.menhir
-    # ocamlPackages.ocamlformat
+    ocamlPackages.ocamlformat
     # ocamlPackages.ppx_compare
     # ocamlPackages.ppx_deriving
     # ocamlPackages.ppx_jane
