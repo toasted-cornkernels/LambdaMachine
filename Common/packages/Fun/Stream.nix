ff1d748417e9396409ffd5301246bbc4d3492148
toasted-cornkernels <volcano0909@gmail.com>
2026-01-06 20:03:29 -0500

    update

--- Common/packages/Fun/Stream.nix
+++ Common/packages/Fun/Stream.nix
@@ -2,8 +2,8 @@
   home.packages = with pkgs; [
     streamlink
     yt-dlp
-    perl538Packages.WWWYoutubeViewer
-    perl538Packages.LaTeXML
+    perl5Packages.WWWYoutubeViewer
+    perl5Packages.LaTeXML
   ];
 }
 
